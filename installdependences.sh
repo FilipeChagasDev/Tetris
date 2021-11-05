2f6cb2a26ceb6a9da22a40b21600e78490d737ce
filipechagasdev <filipe.ferraz0@gmail.com>
2021-11-05 00:56:11 -0400

    Dependences updated

--- installdependences.sh
+++ installdependences.sh
@@ -1,8 +1,14 @@
 apt install g++
+apt install libgl-dev
 apt install mesa-common-dev
 apt install libmesa-dev
 apt install libglfw3
 apt install libglfw3-dev
+apt install libglu1-mesa-dev 
+apt install freeglut3-dev
+apt install libfreetype-dev 
+apt install libfreetype6 
+apt install libfreetype6-dev
 
 git clone https://github.com/jtsiomb/libdrawtext.git
 cd libdrawtext
